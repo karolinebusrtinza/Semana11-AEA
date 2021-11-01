bc8ceda1534f90aff50b808aab92d2a39059b077
karoline bustinza <karoline.bustinza@tecsup.edu.pe>
2021-10-31 22:47:52 -0500

    first commit

---
+++ Semana11-AEA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Semana11_AEA.MvcApplication" Language="C#" %>
---
+++ Semana11-AEA/Models/Enrollment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Semana11_AEA.Models
+{
+    public class Grade
+    {
+        A, B, C, D, F
+    }
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        [DisplayFormat(NullDisplayText = "No grade")]
+        public Grade? Grade { get; set; }
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
+
+    }
+
+   
+}
